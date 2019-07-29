ab582ce90baa4aca28ed0d53b328eeec335c59bd
Sergio Lopez <slopez0286@gmail.com>
2019-07-29 23:19:55 +0000

    Printf

--- _printf.c
+++ _printf.c
@@ -1,63 +1,49 @@
 #include <stdarg.h>
 #include "holberton.h"
-
 /**
 *_printf-Own Printf function
 *
 *@format: Int Value
-*
 *Return: Int value
 */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	unsigned int i = 0, j = 0;
-	int counter = 0;
+	unsigned int i = 0, j = 0, counter = 0;
 
 	format_opt data_t[] = {
 			{"c", print_c},
 			{"s", print_s},
 			{"%", print_mod},
-	/*		{"i",print_i},
-			{"d",print_d},
-			{"b",},
-			{"u",},
-			{"o",},
-			{"x",},
-			{"X",},
-			{"S",},
-			{"p",},
-			{"r",},
-			{"R",},*/
 			{NULL, NULL}
 			};
 	/* initialize valist for num number or argument*/
 	va_start(args, format);
-
 	/* access all the argument assigned to valist*/
-
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
-	while (format && format[i])
+	for (i = 0; format && format[i]; i++)
 	{
-		while (data_t[j].type)
+		if (format[i] != '%')
 		{
-			if (*data_t[j].type == format[i])
-			{
-				data_t[j].f(args);
-				counter += data_t[j].f(args);
-		
-			}
-			j++;
+		counter += _putchar(format[i]);
+		continue;
 		}
-		j = 0;
+		for (j = 0; data_t[j].type; j++)
+		{
+			if (*data_t[j].type == format[i + 1])
+			{
+				counter += data_t[j].f(args);
+				break;
+			}
+		}
 		i++;
+		if (format[j])
+		{
+			counter += _putchar('%');
+			counter += _putchar(format[i]);
+		}
 	}
-	
-	return (counter);
-	/*clen memory reserved for valist */
-
 	va_end(args);
+	return (counter);
 }
