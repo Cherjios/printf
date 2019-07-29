93d9e4d9cc53da8e35d5d121932fdf7fd66e65ed
Hanhuyeny2k <hanhuyeny2k@gmail.com>
2019-07-29 23:53:55 +0000

    functions2

--- functions2.c
+++ functions2.c
@@ -1,21 +1,10 @@
 #include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ *print_c - main entry, helper function, print character
+ *@args: arguments from the list
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *Return: the number of character printed, excluding the null byte.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
-  *print_c: main entry, helper function, print character
-  *@args- arguments from the list
-  *
-  *return- the number of character printed, excluding the null byte.
-  */
 int print_c(va_list args)
 {
 	_putchar(va_arg(args, int));
@@ -23,11 +12,11 @@ int print_c(va_list args)
 }
 
 /**
-  *print_s: main entry, helper function, print string
-  *@args- arguments from the list
-  *
-  *return- the number of character printed, excluding the null byte.
-  */
+ *print_s - main entry, helper function, print string
+ *@args: arguments from the list
+ *
+ *Return: the number of character printed, excluding the null byte.
+ */
 int print_s(va_list args)
 {
 	char *str;
@@ -47,67 +36,46 @@ int print_s(va_list args)
 }
 
 /**
-  *print_mod: main entry, helper function, print percent sign
-  *@args- arguments from the list
-  *
-  *return- the number of character printed, excluding the null byte.
-  */
+ *print_mod - main entry, helper function, print percent sign
+ *@args: arguments from the list
+ *
+ *Return: the number of character printed, excluding the null byte.
+ */
 int print_mod(va_list args)
 {
 	(void) args;
-		_putchar('%');
+	_putchar('%');
 	return (1);
 }
 
 /**
-  *print_i: main entry, helper function, print integer
-  *@args- arguments from the list
-  *
-  *return- the number of character printed, excluding the null byte.
-  */
-/*
+ *print_i - main entry, helper function, print integer
+ *@args: arguments from the list
+ *
+ *Return: the number of character printed, excluding the null byte.
+ */
+
 int print_i(va_list args)
 {
 	int n = va_arg(args, int);
 	int j = 0;
 
-	if (n > 9)
-	{
-		print_i(n / 10);
-		j += _putchar(n % 10 + '\0');
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-		print_i(n / -10);
-		j += _putchar(n % 10 + '\0');
-	}
+	j = print_number(n);
 	return (j);
 }
 
-**
-  *print_d: main entry, helper function, print digit
-  *@args- arguments from the list
-  *
-  *return- the number of character printed, excluding the null byte.
-  */
-/*
-int print_d(va_list args, int)
+/**
+ *print_d - main entry, helper function, print digit
+ *@args: arguments from the list
+ *
+ *Return: the number of character printed, excluding the null byte.
+ */
+
+int print_d(va_list args)
 {
 	int n = va_arg(args, int);
 	int j = 0;
 
-	if (n > 9)
-	{
-		print_d(n / 10);
-		j += _putchar(n % 10 + '\0');
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-		print_d(n / -10);
-		j += _putchar(n % 10 + '\0');
+	j = print_number(n);
 	return (j);
 }
-*/
-
